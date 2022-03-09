18b91843f78297bbd8726f57ffebbd921fbbbca9
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-03-09 17:41:50 +0530

    getting the time stamps

--- background.js
+++ background.js
@@ -4,4 +4,12 @@ chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.sync.set({color});
     console.log('Default background color set to %cgreen',`color:${color}`);
 
+});
+
+chrome.action.onClicked.addListener((tab)=>{
+    console.log("im");
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content-script.js']
+      });
 })
--- popup.js
+++ popup.js
@@ -1,19 +1,61 @@
 let changeColor=document.getElementById('changeColor');
+
+const d=async ()=>{
+    let [tab]=await chrome.tabs.query({active:true,currentWindow:true});
+    
+chrome.scripting.executeScript({
+    target:{tabId:tab.id},
+    
+    function:setPageBackgroundColor,
+});
+}
+
+d();
+
+
+
 changeColor.addEventListener('click',async ()=>{
     let [tab]=await chrome.tabs.query({active:true,currentWindow:true});
     
     chrome.scripting.executeScript({
         target:{tabId:tab.id},
+        
         function:setPageBackgroundColor,
     })
 })
 
 function setPageBackgroundColor(){
+    console.log("Here");
     chrome.storage.sync.get("color",({color})=>{
         document.body.style.backgroundColor=color;
         document.body.style.color='pink';
     });
+    const bar=document.body.getElementsByClassName('ytp-progress-bar');
+    console.log(bar[0]);
+    const controls=document.body.getElementsByClassName('ytp-left-controls');
+    const div=document.createElement('div');
+    div.innerText="SastaNotion";
+    
+    controls[0].append(div);
+    bar[0].addEventListener('mousemove',()=>{
+        const div=document.createElement('div');
+        // div.style.height='50px';
+        // div.style.backgroundColor='red';
+        bar[0].append(div);
+        console.log("hovering");
+    })
+
+    bar[0].addEventListener('mouseout',()=>{
+        
+        console.log("hovering");
+    })
+    div.addEventListener('click',()=>{
+        console.log("here2")
+        console.log(bar[0]);
+        const time=document.body.getElementsByClassName('ytp-time-current');
+        console.log(time[0].innerText);
+    });
 }
 chrome.storage.sync.get("color",({color})=>{
     changeColor.style.backgroundColor=color;
-})
+});
